10cc9509dad50e46311ba7e23baa0ed17b154882
qqqbin <kkb6964@naver.com>
2022-11-22 11:17:28 +0900

    allowed_host

--- pawPrj/settings.py
+++ pawPrj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+v4%ym=sx6)dl*qlme55=yv%r+yak5o^3d-wssu&o9$-%ip6a^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '*'
+    '.ap-northeast-2.compute.amazonaws.com'
 ]
 
 # Application definition
